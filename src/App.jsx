1b000cd6c0ccbff787b979c8d6dd03d452d05cfa
christian <Christianmnaira@gmail.com>
2025-12-01 17:17:00 +0800

    feat: initialize the main component page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-
+import { useEffect, useState } from "react"
+import { listItems } from "./services/itemServices";
 
 function App() {
 
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getAllItems();
+  }, []);
+
+  const getAllItems = () => {
+
+    listItems().then((res) => {
+      // console.log(res.data);
+      setItems(res.data);
+    }).catch((err) => console.error(err));
+
+  }
+
   return (
     <>
-    asdasd
+    <h1>Hello World</h1>
+    <div>
+      {items.map((item, index) => (
+        <div key={index}>
+          <h2>{item.taskTitle}</h2>
+          <p>{item.taskDescription}</p>
+        </div>
+      ))}
+    </div>
     </>
   )
 }
