b46c8feb3753d387927f00388b52c29cbac47744
christian <Christianmnaira@gmail.com>
2025-12-01 17:09:46 +0800

    feat: added an REST API services

---
+++ src/services/itemServices.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const REST_API_BASE_URL = "http://localhost:8080/items";
+
+// adding an item
+export const addItem = (item) => axios.post(REST_API_BASE_URL, item);
+
+// deleting an item
+export const deleteItem = (itemId) => axios.post(REST_API_BASE_URL + "/" + itemId);
+
+// listing all items
+export const listItems = () => axios.get(REST_API_BASE_URL);
+
+// getting an item by id
+export const getItem = (itemId) => axios.get(REST_API_BASE_URL + "/" + itemId, item);
+
+// updating an item by id
+export const updateItem = (itemId, item) => axios.put(REST_API_BASE_URL + "/" + itemId, item);
+
+
