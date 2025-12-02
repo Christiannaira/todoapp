1c2090e52afeb1fb09ac468b49bccf7dfee14815
christian <Christianmnaira@gmail.com>
2025-12-02 18:06:37 +0800

    feat: implemented add service functionality

--- src/components/cards/TaskAdding.jsx
+++ src/components/cards/TaskAdding.jsx
@@ -1,20 +1,40 @@
 import { useState } from "react";
 import { GoPlus } from "react-icons/go";
+import { addItem } from "../../services/itemServices";
 
 export const TaskAdding = () => {
 
     const [taskTitle, setTaskTitle] = useState("");
     const [taskDescription, setTaskDescription] = useState("");
 
-    const handleTaskSubmit = (e) => {
+    // adding an item
+    const handleAddItem = (e) => {
         e.preventDefault();
-        
-        const taskEntry = {
-            taskTitle,
-            taskDescription,
+
+        if (validateItem(taskTitle, taskDescription)) {
+
+            // object to be sent to the backend
+            const itemEntry = {
+                taskTitle,
+                taskDescription,
+            };
+
+            // service
+            addItem(itemEntry).then((res) => {
+                alert("Successfully added an item");
+                getAllItems();
+            }).catch((err) => console.error(err));
+
+        } else {
+            alert("Please enter your task title and task description")
         }
 
-        console.log(taskEntry);
+    }
+
+    // filtering input forms
+    const validateItem = (itemTitle, itemDescription) => {
+        if (itemTitle === "" || itemDescription === "") return false;
+        return true;
     }
 
     return (
@@ -27,16 +47,16 @@ export const TaskAdding = () => {
                             Add Task
                         </button>
                     </div>
-                    <form onSubmit={handleTaskSubmit}>
+                    <form onSubmit={handleAddItem}>
                         <div className="bg-[#242424] rounded-md relative px-6 py-5 flex flex-col gap-3">
                             <input type="text" placeholder="Create Task Title"
-                                className="text-xl p-1" onChange={(e) => setTaskTitle(e.target.value)} value={taskTitle}/>
-                            <input type="text" placeholder="Create Task Description" className="text-[#5B5B5B] text-sm p-1" onChange={(e) => setTaskDescription(e.target.value)} value={taskDescription}/>
+                                className="text-xl p-1" onChange={(e) => setTaskTitle(e.target.value)} value={taskTitle} />
+                            <input type="text" placeholder="Create Task Description" className="text-[#5B5B5B] text-sm p-1" onChange={(e) => setTaskDescription(e.target.value)} value={taskDescription} />
                             <div>
                                 <button type="submit" className="bg-[#336AE0] rounded-md px-4 py-1 cursor-pointer">Create Task</button>
                             </div>
                         </div>
-                        
+
                     </form>
 
                 </div>
