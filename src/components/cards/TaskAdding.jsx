49963b3e8cb8b480253345784d48133c617479d6
Christian <Christianmnaira@gmail.com>
2025-12-06 17:38:22 +0800

    feat: initialize the pop up for adding an item

--- src/components/cards/TaskAdding.jsx
+++ src/components/cards/TaskAdding.jsx
@@ -4,90 +4,149 @@ import { addItem, listItems } from "../../services/itemServices";
 import { TaskAddedItem } from "./TaskAddedItem";
 
 export const TaskAdding = () => {
+   const [taskTitle, setTaskTitle] = useState("");
+   const [taskDescription, setTaskDescription] = useState("");
 
-    const [taskTitle, setTaskTitle] = useState("");
-    const [taskDescription, setTaskDescription] = useState("");
+   const [allItems, setAllItems] = useState([]);
 
-    const [allItems, setAllItems] = useState([]);
+   const [openAddTask, setOpenAddTask] = useState(false);
 
-    useEffect(() => {
-        getAllItems();
-    }, []);
+   useEffect(() => {
+      getAllItems();
+   }, []);
 
-    // adding an item
-    const handleAddItem = (e) => {
-        e.preventDefault();
+   // adding an item
+   const handleAddItem = (e) => {
+      e.preventDefault();
 
-        if (validateItem(taskTitle, taskDescription)) {
+      if (validateItem(taskTitle, taskDescription)) {
+         // object to be sent to the backend
+         const itemEntry = {
+            taskTitle,
+            taskDescription,
+         };
 
-            // object to be sent to the backend
-            const itemEntry = {
-                taskTitle,
-                taskDescription,
-            };
+         // service
+         addItem(itemEntry)
+            .then((res) => {
+               // alert("Successfully added an item");
+               getAllItems();
+            })
+            .catch((err) => console.error(err));
 
-            // service
-            addItem(itemEntry).then((res) => {
-                // alert("Successfully added an item");
-                getAllItems();
-            }).catch((err) => console.error(err));
+         // emptying input fields
+         setTaskTitle("");
+         setTaskDescription("");
+      } else {
+         alert("Please enter your task title and task description");
+      }
+   };
 
-            // emptying input fields
-            setTaskTitle("");
-            setTaskDescription("");
+   // filtering input forms
+   const validateItem = (itemTitle, itemDescription) => {
+      if (itemTitle === "" || itemDescription === "") return false;
+      return true;
+   };
 
-        } else {
-            alert("Please enter your task title and task description")
-        }
-
-    }
-
-    // filtering input forms
-    const validateItem = (itemTitle, itemDescription) => {
-        if (itemTitle === "" || itemDescription === "") return false;
-        return true;
-    }
-
-    const getAllItems = () => {
-        listItems().then((res) => {
+   const getAllItems = () => {
+      listItems()
+         .then((res) => {
             setAllItems(res.data);
-        }).catch((err) => {
+         })
+         .catch((err) => {
             console.error(err);
-        })
-    }
+         });
+   };
 
-    return (
-        <>
-            <div className='w-230 max-w-full max-h-110 overflow-y-scroll px-5'>
-                <div className="flex flex-col gap-3">
-                    <div>
-                        <button className="bg-[#336AE0] rounded-md flex items-center justify-center gap-2 px-6 py-3 cursor-pointer hover:bg-[#225DDC] transition-all duration-100 ease text-sm">
-                            <GoPlus size={20} />
-                            Add Task
+   const handleOpenAddTask = () => {
+      setOpenAddTask(!openAddTask);
+   };
+
+   return (
+      <>
+         {openAddTask && (
+            <div
+               className="absolute w-full h-full bg-[#282828]/90 z-100"
+               onClick={() => setOpenAddTask(!openAddTask)}
+            />
+         )}
+         {openAddTask && (
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-200 w-240 max-w-full p-5">
+               <form onSubmit={handleAddItem}>
+                  <div className="bg-[#242424] rounded-md relative px-6 py-5 flex flex-col gap-3">
+                     <input
+                        type="text"
+                        placeholder="Create Task Title"
+                        className="text-xl p-1"
+                        onChange={(e) => setTaskTitle(e.target.value)}
+                        value={taskTitle}
+                     />
+                     <input
+                        type="text"
+                        placeholder="Create Task Description"
+                        className="text-[#5B5B5B] text-sm p-1"
+                        onChange={(e) => setTaskDescription(e.target.value)}
+                        value={taskDescription}
+                     />
+                     <div>
+                        <button
+                           type="submit"
+                           className="bg-[#336AE0] rounded-md px-4 py-1 cursor-pointer"
+                        >
+                           Create Task
                         </button>
-                    </div>
-                    <form onSubmit={handleAddItem}>
-                        <div className="bg-[#242424] rounded-md relative px-6 py-5 flex flex-col gap-3">
-                            <input type="text" placeholder="Create Task Title"
-                                className="text-xl p-1" onChange={(e) => setTaskTitle(e.target.value)} value={taskTitle} />
-                            <input type="text" placeholder="Create Task Description" className="text-[#5B5B5B] text-sm p-1" onChange={(e) => setTaskDescription(e.target.value)} value={taskDescription} />
-                            <div>
-                                <button type="submit" className="bg-[#336AE0] rounded-md px-4 py-1 cursor-pointer">Create Task</button>
-                            </div>
-                        </div>
-
-                    </form>
-
-                </div>
-
-                {
-                    allItems?.map((item, index) => (
-                        <TaskAddedItem key={index} item={item} getAllItems={getAllItems}/>
-
-                    ))
-                }
-
+                     </div>
+                  </div>
+               </form>
             </div>
-        </>
-    )
-}
+         )}
+         <div className="w-230 max-w-full max-h-110 overflow-y-scroll px-5">
+            <div className="flex flex-col gap-3">
+               <div>
+                  <button
+                     className="bg-[#336AE0] rounded-md flex items-center justify-center gap-2 px-6 py-3 cursor-pointer hover:bg-[#225DDC] transition-all duration-100 ease text-sm"
+                     onClick={handleOpenAddTask}
+                  >
+                     <GoPlus size={20} />
+                     Add Task
+                  </button>
+               </div>
+               <form onSubmit={handleAddItem}>
+                  <div className="bg-[#242424] rounded-md relative px-6 py-5 flex flex-col gap-3">
+                     <input
+                        type="text"
+                        placeholder="Create Task Title"
+                        className="text-xl p-1"
+                        onChange={(e) => setTaskTitle(e.target.value)}
+                        value={taskTitle}
+                     />
+                     <input
+                        type="text"
+                        placeholder="Create Task Description"
+                        className="text-[#5B5B5B] text-sm p-1"
+                        onChange={(e) => setTaskDescription(e.target.value)}
+                        value={taskDescription}
+                     />
+                     <div>
+                        <button
+                           type="submit"
+                           className="bg-[#336AE0] rounded-md px-4 py-1 cursor-pointer"
+                        >
+                           Create Task
+                        </button>
+                     </div>
+                  </div>
+               </form>
+            </div>
+
+            {allItems?.map((item, index) => (
+               <TaskAddedItem
+                  key={index}
+                  item={item}
+                  getAllItems={getAllItems}
+               />
+            ))}
+         </div>
+      </>
+   );
+};
