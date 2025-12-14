1e6a2602e5f23e4d7eacdf0807de54886801905c
Christian <Christianmnaira@gmail.com>
2025-12-14 22:34:58 +0800

    feat: displaying the task level on added item

--- src/components/cards/TaskAddedItem.jsx
+++ src/components/cards/TaskAddedItem.jsx
@@ -14,6 +14,15 @@ export const TaskAddedItem = ({ item, getAllItems }) => {
       <>
          <div>
             <div className="bg-[#242424] rounded-md relative px-6 py-5 flex flex-col gap-3 my-3 cursor-pointer relative">
+               <div
+                  className={`absolute top-0 left-0 w-5 h-5 rounded-sm ${
+                     item.priorityLevel === "1"
+                        ? `bg-red-500`
+                        : item.priorityLevel === "2"
+                        ? `bg-green-500`
+                        : `bg-yellow-500`
+                  }`}
+               />
                <h3 className="text-2xl text-[#B0B0B0]">{item.taskTitle}</h3>
                <span className="text-[#5B5B5B] text-md">
                   {item.taskDescription}
